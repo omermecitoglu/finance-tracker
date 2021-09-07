1b7cde78b28e2273657e758598b4fe1f1bff5c1a
Omer Mecitoglu <omer.mecitoglu@gmail.com>
2021-09-07 20:45:42 +0300

    moved App component

---
+++ src/components/app.jsx
@@ -0,0 +1,9 @@
+import React from "react";
+import { Container } from "@material-ui/core";
+
+const App = () => (
+	<Container>
+	</Container>
+);
+
+export default App;
--- src/index.jsx
+++ src/index.jsx
@@ -2,13 +2,7 @@ import React from "react";
 import ReactDOM from "react-dom";
 import { Provider } from "react-redux";
 import { store } from "./core";
-import { Container, Button } from "@material-ui/core";
-
-const App = () => (
-	<Container>
-		<Button variant="contained" color="primary">Hello World</Button>
-	</Container>
-);
+import App from "./components/app";
 
 const body = document.getElementsByTagName("body")[0];
 const root = document.createElement("div");
