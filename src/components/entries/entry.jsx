fc3115c4b3996117640674b42a25cd75763082fa
Omer Mecitoglu <omer.mecitoglu@gmail.com>
2021-09-07 22:49:03 +0300

    updated entry component (added target currency)

--- src/components/entries/entry.jsx
+++ src/components/entries/entry.jsx
@@ -1,16 +1,21 @@
 import React from "react";
+import { connect } from "react-redux";
 import { TableRow, TableCell, Tooltip } from "@material-ui/core";
 import { price } from "./utils";
 
-const Entry = ({ name, plus, amount, currency }) => (
+const Entry = ({ name, plus, amount, currency, targetCurrency }) => (
 	<TableRow>
 		<TableCell component="th" scope="row">{name}</TableCell>
 		<TableCell align="right">
-			<Tooltip title={price(plus, amount, currency)}>
-				<span>{price(plus, amount, currency)}</span>
+			<Tooltip title={currency !== targetCurrency ? price(plus, amount, currency) : ""}>
+				<span>{price(plus, amount, targetCurrency)}</span>
 			</Tooltip>
 		</TableCell>
 	</TableRow>
 );
 
-export default Entry;
+const mstp = ({ currency }) => ({
+	targetCurrency: currency.current,
+});
+
+export default connect(mstp, null)(Entry);
