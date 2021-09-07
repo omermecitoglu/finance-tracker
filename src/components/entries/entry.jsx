b7f14431b9dd7ace06192eee7bfdd0d1343932a9
Omer Mecitoglu <omer.mecitoglu@gmail.com>
2021-09-08 00:39:37 +0300

    updated entry component (exchange)

--- src/components/entries/entry.jsx
+++ src/components/entries/entry.jsx
@@ -1,21 +1,22 @@
 import React from "react";
 import { connect } from "react-redux";
 import { TableRow, TableCell, Tooltip } from "@material-ui/core";
-import { price } from "./utils";
+import { price, convert } from "./utils";
 
-const Entry = ({ name, plus, amount, currency, targetCurrency }) => (
+const Entry = ({ name, plus, amount, currency, targetCurrency, exchangeRates }) => (
 	<TableRow>
 		<TableCell component="th" scope="row">{name}</TableCell>
 		<TableCell align="right">
 			<Tooltip title={currency !== targetCurrency ? price(plus, amount, currency) : ""}>
-				<span>{price(plus, amount, targetCurrency)}</span>
+				<span>{price(plus, convert(exchangeRates, currency, targetCurrency, amount), targetCurrency)}</span>
 			</Tooltip>
 		</TableCell>
 	</TableRow>
 );
 
-const mstp = ({ currency }) => ({
+const mstp = ({ currency, exchange }) => ({
 	targetCurrency: currency.current,
+	exchangeRates: exchange,
 });
 
 export default connect(mstp, null)(Entry);
