c1467463eef8a179230ad9e2dae35255d8816ece
Omer Mecitoglu <omer.mecitoglu@gmail.com>
2021-09-07 15:34:51 +0300

    added material-ui

--- src/index.jsx
+++ src/index.jsx
@@ -1,8 +1,11 @@
 import React from "react";
 import ReactDOM from "react-dom";
+import { Container, Button } from "@material-ui/core";
 
 const App = () => (
-	<span>Hello World</span>
+	<Container>
+		<Button variant="contained" color="primary">Hello World</Button>
+	</Container>
 );
 
 const body = document.getElementsByTagName("body")[0];
